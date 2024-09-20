f0430e7cc72c75dd69840d8b10ac974bbb4a1b81
Jeff Perrin <jeffreyperrin10@gmail.com>
2024-09-20 17:20:28 -0400

    Update game.rb through TDD

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,62 @@
 # frozen_string_literal: true
 
 class Game
+  attr_reader :first_player, :second_player, :grid
+
+  def initialize
+    @first_player = nil
+    @second_player = nil
+    @grid = Grid.new
+    @current_player = nil
+  end
+
+  def play
+    game_set_up
+    grid.show
+    take_turns
+  end
+
+  def create_player(number, game_piece)
+    puts "Player #{number} will use '#{game_piece}' as their game piece."
+    puts "What's the name for player #{number}?"
+    name = gets.chomp
+    Player.new(name, game_piece)
+  end
+  
+  def turn(player)
+    column = move_input(player)
+    grid.update_grid(player.game_piece, column)
+    grid.show
+  end
+
+  private
+
+  def game_set_up
+    puts "Let's play a game of Connect Four in the command line"
+    @first_player = create_player(1, 'X')
+    @second_player = create_player(2, 'O')
+  end
+
+  def take_turns
+    @current_player = first_player
+    until grid.full?
+      turn(current_player)
+      break if grid.winner?
+
+      @current_player = switch_current_player
+    end
+  end
+
+  def move_input(player)
+    puts "#{player.name}, please pick a column to drop your game piece (#{player.game_piece})"
+    input = gets.chomp.to_i
+    return input if grid.valid_column?(input)
+
+    puts "That's not valid"
+    move_input(player)
+  end
+
+  def switch_current_player
+    current_player == first_player ? second_player : first_player
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,13 +1,48 @@
 # frozen_string_literal: true
 
 require_relative '../lib/game'
+require_relative '../lib/grid'
+require_relative '../lib/player'
 
 describe Game do
   subject(:game) { described_class.new }
 
+  # before do
+  #   game.instance_variable_set(:@grid, instance_double(Grid))
+  # end
+
   describe '#play' do
     it 'shows the grid' do
+      allow(game).to receive(:game_set_up)
+      allow(game).to receive(:take_turns)
       expect(game.grid).to receive(:show)
+      game.play
+    end
+  end
+
+  describe '#create_player' do
+    it 'creates player 1' do
+      player_name = 'bob'
+      player_piece = 'X'
+      allow(game).to receive(:puts).twice
+      allow(game).to receive(:gets).and_return(player_name)
+      expect(Player).to receive(:new).with(player_name, player_piece)
+      game.create_player(1, 'X')
+    end
+  end
+
+  describe '#turn' do
+    let(:player) { double('player', name: 'bob', game_piece: 'X')}
+
+    context 'when grid is empty' do
+      it 'updates the grid' do
+        player_column_input = 1
+        allow(game).to receive(:move_input).and_return(player_column_input)
+        expect(game.grid).to receive(:update_grid).with(player.game_piece, player_column_input)
+        game.turn(player)
+      end
     end
   end
 end
+
+
